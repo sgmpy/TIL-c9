1fed120fbb6ec7efe65e1918bf80b2723da50b54
Junwoo Hwang <no.water.in.the.house@gmail.com>
2019-01-29 08:34:48 +0000

    D20 | 190129 PM | CRUD

--- django/crud/posts/views.py
+++ django/crud/posts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Post
 
 # Create your views here.
@@ -7,14 +7,15 @@ def new(request):
     return render(request, 'new.html')
 
 def create(request):
-    title = request.GET.get('title')
-    content = request.GET.get('content')
+    # request.GET / request.POST
+    title = request.POST.get('title')
+    content = request.POST.get('content')
     
     # DB INSERT
     post = Post(title=title, content=content)
     post.save()
         
-    return render(request, 'create.html')
+    return redirect(f'/posts/{post.pk}/')
     
 def index(request):
     # All Post
@@ -22,7 +23,36 @@ def index(request):
     
     return render(request, 'index.html', {'posts':posts})
 
+def detail(request, post_id):
+    post = Post.objects.get(pk=post_id)
+    return render(request, 'detail.html', {'post': post})
+
+def delete(request, post_id):
+    post = Post.objects.get(pk=post_id)
+    post.delete()
+    return redirect('/posts/')
+    
+def edit(request, post_id):
+    post = Post.objects.get(pk=post_id)
+    return render(request, 'edit.html', {'post':post})
+    
+def update(request, post_id):
+    post = Post.objects.get(pk=post_id)
+    post.title = request.POST.get('title')
+    post.content = request.POST.get('content')
+    post.save()
+    
+    return redirect(f'/posts/{post_id}/')
 
 
 
 
+
+
+
+
+# def naver(request, query):
+#     return redirect(f'https://search.naver.com/search.naver?query={query}')
+
+# def github(request, username):
+#     return redirect(f'https://github.com/{username}')
