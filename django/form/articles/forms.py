0b03ce260b0c98a2de827f311dd4f739d8b43b4d
Junwoo Hwang <no.water.in.the.house@gmail.com>
2019-02-11 08:46:39 +0000

    D26 | 190211 PM | Django Form

---
+++ django/form/articles/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from .models import Article
+
+
+class ArticleForm(forms.Form):
+    title = forms.CharField(label='제목')
+    content = forms.CharField(label='내용', widget=forms.Textarea(attrs={
+        'rows': 5,
+        'cols': 50,
+        'placeholder': '내용을 입력하세요.',
+    }))
+    
+
+class ArticleModelForm(forms.ModelForm):
+    title = forms.CharField(label='제목')
+    
+    class Meta:
+        model = Article
+        fields = ['title', 'content']
+
