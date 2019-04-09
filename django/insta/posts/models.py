a29984728e0cfe4c3153aa2ba83489171656a752
Junwoo Hwang <no.water.in.the.house@gmail.com>
2019-04-09 08:44:08 +0000

    190409 | Insta - Image upload

--- django/insta/posts/models.py
+++ django/insta/posts/models.py
@@ -1,5 +1,17 @@
 from django.db import models
+from imagekit.models import ProcessedImageField
+from imagekit.processors import ResizeToFill
+
+def post_image_path(instance, filename):
+    return f'posts/images/{filename}'   
 
 # Create your models here.
 class Post(models.Model):
     content = models.TextField()
+    # image = models.ImageField(blank=True)
+    image = ProcessedImageField(
+                    upload_to=post_image_path, # 저장 위치
+                    processors=[ResizeToFill(600,600)], # 처리할 작업 목록
+                    format='JPEG', # 저장 포맷
+                    options={'quality':90}, # 옵션
+                )
--- django/insta/posts/views.py
+++ django/insta/posts/views.py
@@ -9,13 +9,25 @@ def list(request):
 
 def create(request):
     if request.method == 'POST':
-        post_form = PostForm(request.POST)
+        post_form = PostForm(request.POST, request.FILES)
         if post_form.is_valid():
             post_form.save()
             return redirect('posts:list')
     else:
         post_form = PostForm()
     return render(request, 'posts/create.html', {'post_form': post_form})
+
+
+def update(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    if request.method == 'POST':
+        post_form = PostForm(request.POST, request.FILES, instance=post)
+        if post_form.is_valid():
+            post_form.save()
+            return redirect('posts:list')
+    else:
+        post_form = PostForm(instance=post)
+    return render(request, 'posts/create.html', {'post_form': post_form}) 
 
 
 def delete(request, post_id):
